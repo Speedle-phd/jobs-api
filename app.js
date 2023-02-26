962b0cecf1ec6a40942de4337d30a7792086436c
Emanuel Eirich <shanks-@gmx.de>
2023-02-26 12:14:46 +0100

    swagger yaml

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
 const rateLimit = require('express-rate-limit')
+const swagger = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDocument = yaml.load("./swagger.yaml")
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -22,9 +25,11 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 // routes
+
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">API DOCS</a>');
 });
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/job', auth, jobRouter)
